3622f0e5bfbbc0ad8f7257072e6cd120661c72f8
AndriikoPe <andrusha20022002@gmail.com>
2024-03-15 21:42:04 +0200

    Add Person model class

---
+++ Model/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace permiakov_lab2.Model
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
