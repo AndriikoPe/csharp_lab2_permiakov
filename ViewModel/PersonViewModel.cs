b6146d8b57cfe867bceee175014d87bc9d92b09b
AndriikoPe <andrusha20022002@gmail.com>
2024-03-16 10:22:00 +0200

    Add empty view model, implement ui

---
+++ ViewModel/PersonViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace permiakov_lab2.ViewModel
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
