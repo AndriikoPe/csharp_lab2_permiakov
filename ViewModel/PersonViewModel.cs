30b5b205dcb0630be46b1dc5c9be8b612d5931fa
AndriikoPe <andrusha20022002@gmail.com>
2024-03-16 10:50:49 +0200

    wip

--- ViewModel/PersonViewModel.cs
+++ ViewModel/PersonViewModel.cs
@@ -1,9 +1,59 @@
-﻿using System.ComponentModel;
+﻿using permiakov_lab2.Command;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace permiakov_lab2.ViewModel
 {
-    public class PersonViewModel : INotifyPropertyChanged
+    internal class PersonViewModel : INotifyPropertyChanged
     {
+        private string firstName;
+        private string lastName;
+        private string emailAddress;
+        private DateTime? dateOfBirth;
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstName
+        {
+            get => firstName;
+            set { firstName = value; OnPropertyChanged(nameof(CanProceed)); }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set { lastName = value; OnPropertyChanged(nameof(CanProceed)); }
+        }
+
+        public string EmailAddress
+        {
+            get => emailAddress;
+            set { emailAddress = value; OnPropertyChanged(nameof(CanProceed)); }
+        }
+
+        public DateTime? DateOfBirth
+        {
+            get => dateOfBirth;
+            set { dateOfBirth = value; OnPropertyChanged(nameof(CanProceed)); }
+        }
+
+        public bool CanProceed => !string.IsNullOrWhiteSpace(FirstName)
+            && !string.IsNullOrWhiteSpace(LastName)
+            && !string.IsNullOrWhiteSpace(EmailAddress)
+            && DateOfBirth.HasValue;
+
+        public RelayCommand ProceedCommand => new(
+            execute => ShowStatistics(),
+            canExecute => { return true; });
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ShowStatistics() {
+            
+        }
     }
 }
